2c2b14e3c73f2b36861143791fe8ca17cf82a108
emanuel-goncalves <82672054+emanuel-goncalves@users.noreply.github.com>
2024-12-09 15:30:18 +0000

    unit tests campaign

--- tests/app.test.js
+++ tests/app.test.js
@@ -38,3 +38,25 @@ describe('GET /', () => {
         expect(response.text).toBe('Hello from our server!');
     });
 });
+
+describe('GET /campaign', () => {
+    it('should return all campaigns', async () => {
+        const response = await request(app).get('/campaign');
+        // Print the response body to the console
+        console.log('Response Body:', response.body);
+        // Assert status code is 200
+        // expect(response.status).toBe(200);
+        // Assert response body is an array
+        // expect(Array.isArray(response.body)).toBe(true);
+        // Assert there are 2 campaigns returned
+        // expect(response.body).toHaveLength(2);
+
+        // Assert the first campaign has the expected properties
+        // expect(response.body[0]).toHaveProperty('title', 'Campaign 1');
+        // expect(response.body[0]).toHaveProperty('user_name', 'User 1');
+        // expect(response.body[0]).toHaveProperty('category', 'Health');
+        // expect(response.body[0]).toHaveProperty('priority', 'normal');
+        //expect(response.body[0]).toHaveProperty('objective', 1000);
+        //expect(response.body[0]).toHaveProperty('state', 'active');
+    });
+});
